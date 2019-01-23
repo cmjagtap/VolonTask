093cebd370128e5c1f4e69ac70be2491eb2b7c1b
Chandramohan Jagtap <cmjagtap1@gmail.com>
2019-01-24 01:06:20 +0530

    add files

---
+++ README.md
@@ -0,0 +1,38 @@
+# Twitter API and Elasticsearch
+
+#### AUTHOR:
+
+- [Chandramohan Jagtap](https://github.com/cmjagtap "Chandramohan's github profile")
+
+![Screenshot](screenshots/graph.png)
+
+#### DESCRIPTION:
+
+  - Dataset:  
+        Collected latest tweets from tweeter using API.
+
+  - Data Pre-processing:  
+        From collected data i have separted tweet id, tweet date time and text of tweet
+
+  - Tasks:  
+        Find out donald trump latest tweets using twitter API and dump them into elasticsearch
+  
+  - Solution: 
+        I have design module for genral use we can find any twitter account tweets by providing username to 
+        authenticateUser() method and follwing are sub-task done.
+        1) Authenticate user via API.
+        2) get Tweets from api and created dictionary to store temporary.
+        3) Created Indexing to map reocrds records and dump into elasticsearch.
+        4) Retrived record using id.
+
+
+#### Requirment:
+
+        1) Install python3 
+        2) python twitter package  install using :-	  ' pip3 install twitter '
+		3) python Elasticsearch package install using:-  ' pip3 install elasticsearch '
+        4) Java version <10 to run elasticsearch 
+
+
+#### Execution :  
+	  ` python3 getTweetsAndDumpElasticSearch.py `
---
+++ getTweetsAndDumpElasticSearch.py
@@ -0,0 +1,58 @@
+'''
+__author__ = "Chandramohan Jagtap"
+__license__ = "GPL"
+__email__ = "cmjagtap1@gmail.com"
+__Date__ = "24-1-2019"
+
+'''
+
+import twitter      #import library
+from elasticsearch import Elasticsearch
+
+def authenticateUser(userName):
+    #follwing is api keys provide your keys to run this project
+    consumer_key=''
+    consumer_secret=''
+    access_token_key=''
+    access_token_secret=''
+    
+    api = twitter.Api(consumer_key=consumer_key,
+    consumer_secret=consumer_secret,
+    access_token_key=access_token_key,
+    access_token_secret=access_token_secret)
+    
+    #print(api.VerifyCredentials())
+
+    latestTweets = api.GetUserTimeline(screen_name = userName) #read latest  tweets
+    tweets = [i.AsDict() for i in latestTweets]
+    
+    tweetList=[]
+    for t in tweets:
+        parseTweets={}
+        parseTweets[t['id']]=(t['created_at'],t["text"])
+        tweetList.append(parseTweets)
+    return tweetList
+
+
+
+def createIndexDumpData(latestT):
+    for i in range(1,len(latestT)):
+        #created index and dump tweets into same index
+        es.index(index="donald",ignore=400,doc_type='docs',body=latestT[i-1],id=i) 
+        
+        #es.indices.get_mapping(index="donald",doc_type="docs") #to check is mapping done
+        return
+
+def getRecord(idx):
+    result = es.get(index="donald",doc_type="docs",id=idx) #find tweet given by id
+
+    return result
+
+
+es = Elasticsearch() #create Object of elastic Search default host=localhost and port is port:9200
+
+latestT=authenticateUser("realDonaldTrump") #authenticate user and provide twitter username to find tweets
+
+createIndexDumpData(latestT) #build and dump tweets into elasticsearch
+
+print (getRecord(1)) #provide id to get record
